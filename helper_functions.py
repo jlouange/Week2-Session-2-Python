5d53e36ff88fc36c1b4bf509446e89a7ca1ae358
jlouange <j.mushumba@alustudent.com>
2026-06-29 23:47:04 +0200

    Changed the structure of the function

--- helper_functions.py
+++ helper_functions.py
@@ -1,8 +1,8 @@
 def validate_input(user_input):
   if isinstance(user_input, str) and user_input.strip():
-    print("The input is validated. it is a string and non-empty")
+    return True
   else:
-    print("Invalid input!")
+    return False
 
 def create_message(name,age):
   print("Hello {}, you are {} years old!".format(name,age))
